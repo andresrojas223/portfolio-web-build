c6fdda6ca5c1bd5fc56697c99fb5c944087dc1e0
Andres Rojas <andresrojas12498706@gmail.com>
2022-02-22 10:42:32 -0500

    first commit

---
+++ precache-manifest.573ebb7c7ef3a71a9425afeadc4894a2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afde50228bd9447ee814131f654b8e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeb2ef740a242b4f7712",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "59aa5464d1b9b918d42d",
+    "url": "/static/css/main.4a4d11c6.chunk.css"
+  },
+  {
+    "revision": "aeb2ef740a242b4f7712",
+    "url": "/static/js/2.11e83f9e.chunk.js"
+  },
+  {
+    "revision": "59aa5464d1b9b918d42d",
+    "url": "/static/js/main.9c2e2bb3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a4796c7bfcfca6815545edc236851413",
+    "url": "/static/media/background.a4796c7b.png"
+  },
+  {
+    "revision": "cc2ff2286b316c729dc1fb71fdd17267",
+    "url": "/static/media/gifs_app.cc2ff228.gif"
+  },
+  {
+    "revision": "0ab0d7b53b4e08d8225a33955e26b96a",
+    "url": "/static/media/gifs_heroes_app.0ab0d7b5.gif"
+  },
+  {
+    "revision": "615cd74f47ac1e7f2b029b072787711b",
+    "url": "/static/media/mi_cv.615cd74f.pdf"
+  },
+  {
+    "revision": "4d036efff6e4783ad425baaa0a461b39",
+    "url": "/static/media/paises_app.4d036eff.gif"
+  }
+]);
